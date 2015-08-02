81460a368192062242d74a341c0b7bbbbc79c041
Daniel <danielsfmendes@gmail.com>
2015-08-02 03:42:01 +0100

    Create profile and fight routes

--- client/events.js
+++ client/events.js
@@ -1,11 +1,11 @@
 Template.fighter.events({
-	'click .button-scout': function(event) {
+	/*'click .button-scout': function(event) {
 		event.preventDefault();
 		Router.go('fighterProfile');
 	},
 	'click .button-fight': function(event) {
 		event.preventDefault();
 		Router.go('fight');
-	}
+	}*/
 });
 
